1c3eb1862cdd812f33cac6ff0ad39d4df9c5ca75
Francolesca <88465161+Francolesca@users.noreply.github.com>
2025-07-30 20:50:22 -0300

    TransfTheDatainWebapi

--- src/Starbucks.Api/Extensions/DatabaseInitializer.cs
+++ src/Starbucks.Api/Extensions/DatabaseInitializer.cs
@@ -1,6 +1,10 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Npgsql.Internal;
 using Npgsql.Replication;
+using Starbucks.Api.Resources;
+using Starbucks.Domain;
 using Starbucks.Persistence;
 
 namespace Starbucks.Api.Extensions;
@@ -10,7 +14,8 @@ public static class DatabaseInitializer
     public static async Task ApplyMigration(
         this IApplicationBuilder builder,
         IWebHostEnvironment? environment
-    ) {
+    )
+    {
         using (var scope = builder.ApplicationServices.CreateScope())
         {
             var service = scope.ServiceProvider;
@@ -19,6 +24,7 @@ public static class DatabaseInitializer
             {
                 var context = service.GetRequiredService<StarbucksDbContext>();
                 await context.Database.MigrateAsync();
+                await SeedData (context, environment);
             }
             catch (System.Exception ex)
             {
@@ -26,5 +32,38 @@ public static class DatabaseInitializer
                 logger.LogError(ex, "Error en la migracion");
             }
         }
+    }
+    private static async Task SeedData(
+        StarbucksDbContext context,
+        IWebHostEnvironment? environment
+    )
+    {
+        if (context.Coffes.Any())
+        {
+            return;
+        }
+        if (environment is null)
+        {
+            throw new Exception("Enviroment is null");
+        }
+
+        var rootPath = environment.ContentRootPath;
+        var fullPathCoffe = Path.Combine(rootPath, "Resources/Coffe.json");
+        var coffeDataText = await File.ReadAllTextAsync(fullPathCoffe);
+        var data = JsonConvert.DeserializeObject<List<CoffeJson>>(coffeDataText)
+            ?? Enumerable.Empty<CoffeJson>();
+
+        var coffes = data.Select(json => new Coffe
+        {
+            Id = json.CoffeId,
+            Name = json.Title!,
+            Description = json.Description,
+            Price = 10,
+            CategoryId = json.Category,
+            Imagen = json.Image,
+        }).ToArray();
+
+        await context.Coffes.AddRangeAsync(coffes);
+        await context.SaveChangesAsync();
     }
 }
